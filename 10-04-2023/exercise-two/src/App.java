a5d663333e5a77b5b978dea4e912bb73ed2906b0
Daniel Sansão Araldi <daniel.araldi@edu.univali.br>
2023-04-10 21:28:11 -0300

    feat: greeting user provided values in commad line

---
+++ 10-04-2023/exercise-two/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bom dia " + args[0] + " você tem " + args[1] + " anos de idade!");
+    }
+}
