f38c79cb7766f8da3c5b81709818eedee2ed4399
Daniel Sansão Araldi <daniel.araldi@edu.univali.br>
2023-04-17 21:21:14 -0300

    feat: exploring String object

---
+++ 17-04-2023/exercise-three/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = "Daniel Sansão Araldi";
+        String[] names = name.split(" ");
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.charAt(4));
+        System.out.println(name.concat(" is a programmer"));
+        System.out.println(name.contains("Daniel"));
+
+        for (String username:names) System.out.println(username);
+    }
+}
