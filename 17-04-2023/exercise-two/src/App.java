ac0edca9614e8a3ab15bc1b28d192d570f6e24ae
Daniel Sansão Araldi <daniel.araldi@edu.univali.br>
2023-04-17 21:00:44 -0300

    feat: created grade

---
+++ 17-04-2023/exercise-two/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String grafic = "";
+
+        for (int i = 0; i < args.length; i++) {
+            int grade = Integer.parseInt(args[i]);
+            for (int j = 0; j < grade; j++) grafic += "*";
+
+            System.out.println("Aluno " + (i + 1) + " " + grafic);
+            grafic = "";
+        }
+    }
+}
